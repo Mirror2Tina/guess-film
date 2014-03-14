84784d70c528a6edfd2d47c70f58913db2a99b81
Mirror <hexllive@live.com>
2014-03-14 13:16:05 +0800

    加入数据

---
+++ data/filmdata.js
@@ -0,0 +1,12 @@
+[
+{'index':'0','con':'fdfdfdfdfdfdfdf','tip':'dfdfdfdfdsfsdf','words':'dfdfdfdf','answer':'fdfdfd','picture':'url'},
+{'index':'1','con':'fdfdfdfdfdfdfdf','tip':'dfdfdfdfdsfsdf','words':'dfdfdfdf','answer':'fdfdfd','picture':'url'},
+{'index':'2','con':'fdfdfdfdfdfdfdf','tip':'dfdfdfdfdsfsdf','words':'dfdfdfdf','answer':'fdfdfd','picture':'url'},
+{'index':'3','con':'fdfdfdfdfdfdfdf','tip':'dfdfdfdfdsfsdf','words':'dfdfdfdf','answer':'fdfdfd','picture':'url'},
+{'index':'4','con':'fdfdfdfdfdfdfdf','tip':'dfdfdfdfdsfsdf','words':'dfdfdfdf','answer':'fdfdfd','picture':'url'},
+{'index':'5','con':'fdfdfdfdfdfdfdf','tip':'dfdfdfdfdsfsdf','words':'dfdfdfdf','answer':'fdfdfd','picture':'url'},
+{'index':'6','con':'fdfdfdfdfdfdfdf','tip':'dfdfdfdfdsfsdf','words':'dfdfdfdf','answer':'fdfdfd','picture':'url'},
+{'index':'7','con':'fdfdfdfdfdfdfdf','tip':'dfdfdfdfdsfsdf','words':'dfdfdfdf','answer':'fdfdfd','picture':'url'},
+{'index':'8','con':'fdfdfdfdfdfdfdf','tip':'dfdfdfdfdsfsdf','words':'dfdfdfdf','answer':'fdfdfd','picture':'url'},
+{'index':'9','con':'fdfdfdfdfdfdfdf','tip':'dfdfdfdfdsfsdf','words':'dfdfdfdf','answer':'fdfdfd','picture':'url'},
+]
