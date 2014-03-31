7cddd78375c08571b63474a2650dbc46b0fbd3df
Mirror <hexllive@live.com>
2014-03-31 16:05:55 +0800

    add grunt file document

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,35 +3,25 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/**/*.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
     requirejs: {
       compile: {
         options: {
           baseUrl: "./",
           mainConfigFile: "main.js",
           name: "main", // assumes a production build using almond
-          out: "file.js"
+          out: "build/guess-film.min.js"
         }
       }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+
 
   // !! This loads the plugin into grunt
   grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['requirejs']);
 
 
 
