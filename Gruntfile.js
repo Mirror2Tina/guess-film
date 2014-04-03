c8e7dc2de63066551b0c68193a6bace0adc63caf
ddyyzq <782302999@qq.com>
2014-04-03 11:06:51 +0800

    Revert "add grunt file document"
    This reverts commit 7cddd78375c08571b63474a2650dbc46b0fbd3df.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,25 +3,35 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/**/*.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
     requirejs: {
       compile: {
         options: {
           baseUrl: "./",
           mainConfigFile: "main.js",
           name: "main", // assumes a production build using almond
-          out: "build/guess-film.min.js"
+          out: "file.js"
         }
       }
     }
   });
 
-
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // !! This loads the plugin into grunt
   grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   // Default task(s).
-  grunt.registerTask('default', ['requirejs']);
+  grunt.registerTask('default', ['uglify']);
 
 
 
