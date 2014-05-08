4c2749aa6807c1b01ff54b35fcb8774beec50177
Mirror <hexllive@live.com>
2014-05-08 15:37:51 +0800

    nodejs 下 require 冲突

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -2,20 +2,20 @@
  * bootstraps angular onto the window.document node
  * NOTE: the ng-app attribute should not be on the index.html when using ng.bootstrap
  */
-define([
-    'require',
-    'angular',
-    'angular-ui-router',
-    'app'
-], function (require, ng) {
-    'use strict';
-
-    /*
+(function(define) {
+define(function(require) {
+'use strict';
+     /*
      * place operations that need to initialize prior to app start here
      * using the `run` function on the top-level module
      */
-
-    require(['domReady!'], function (document) {
+ require(['domReady!'], function (document) {
         //ng.bootstrap(document, ['app']);
+        console.log("app lunch")
     });
+
 });
+})(
+	typeof define === 'function' && define.amd ? define : function (factory) { module.exports = factory(require); }
+	// Boilerplate for AMD and Node
+);
