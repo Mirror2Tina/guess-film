dc810608ef8e49fddbefeef0e011d4bd802a8d92
Mirror <hexllive@live.com>
2014-05-08 15:02:18 +0800

    初始化

--- src/js/main.js
+++ src/js/main.js
@@ -43,7 +43,7 @@ requirejs.config({
     },
 
     deps: [
-        // kick start application... see bootstrap.js
-        './bootstrap'
+        //kick start application... see bootstrap.js
+       './bootstrap'
     ]
 });
